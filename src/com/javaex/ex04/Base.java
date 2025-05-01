36ea0bb9426f57b27e44ecc3fa08e755d5451fda
ahah2025 <adghty61>
2025-05-01 18:03:15 +0900

    2025-05-01

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -1,7 +1,9 @@
+//잘 모르겠음
+
 package com.javaex.ex04;
 
 public class Base {
-	
+		
 	//필드
 	private String day;    
 	private String night;
@@ -12,50 +14,44 @@ public class Base {
 		this.night = night;
 		this.afternoon = afternoon;
 	}
-
-    /// !--------------------------------!
-    public void service(String state) {
-        
-        //코드작성할것
-    }    
-    //// !--------------------------------!
+    
+    //낮
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
 	public void day() {
         System.out.println("낮에는 열심히 수업듣자");
     }
 	
-	public String getDay() {
-		return day;
-	}
-
-	public void setDay(String day) {
-		this.day = day;
-	}
-	
-	//// !--------------------------------!
-    public void night() {
-        
-    	//코드작성할것
-    }	
-    
+	//밤
 	public String getNight() {
 		return night;
 	}
 	public void setNight(String night) {
 		this.night = night;
 	}
-
-    public void afternoon(){
-        System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
-    }	
-	
+    public void night() {
+    	System.out.println("밤에는 숙면");
+    }
+    
+	//오후
 	public String getAfternoon() {
 		return afternoon;
 	}
-
 	public void setAfternoon(String afternoon) {
 		this.afternoon = afternoon;
-	}
-
-
+	}	
+    public void afternoon(){
+        System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+    }
+   
+    public void service(String state) {
+    	System.out.println("낮에는 열심히 수업듣자");
+    	System.out.println("밤에는 숙면");
+    	System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+    }    
 }
 
